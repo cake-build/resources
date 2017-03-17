27fba68a821b6b3bf5dac1d5834255399cb979e0
Mattias Karlsson <matkar@wcom.se>
2017-03-17 11:45:23 +0100

    (GH-26) Added AppVeyor & Travis test

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@
 # Define directories.
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TOOLS_DIR=$SCRIPT_DIR/tools
-ADDINS_DIR=$SCRIPT_DIR/addins
-MODULES_DIR=$SCRIPT_DIR/modules
+ADDINS_DIR=$TOOLS_DIR/addins
+MODULES_DIR=$TOOLS_DIR/modules
 NUGET_EXE=$TOOLS_DIR/nuget.exe
 CAKE_EXE=$TOOLS_DIR/Cake/Cake.exe
 PACKAGES_CONFIG=$TOOLS_DIR/packages.config
