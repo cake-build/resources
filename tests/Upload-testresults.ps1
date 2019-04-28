f9eaef325ce50a7dae92902501bf09e48a75b7cc
Sebastian Schauer <makaanneo@users.noreply.github.com>
2019-04-28 19:02:48 +0200

    (tests) Add missing file.

---
+++ tests/Upload-testresults.ps1
@@ -0,0 +1,6 @@
+$webclient = (New-Object 'System.Net.WebClient');
+
+foreach ($testResult in  (Get-ChildItem -Path $testresultFolder)) {
+    "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)" | Write-Output
+    $webclient.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $testResult.FullName));
+}
