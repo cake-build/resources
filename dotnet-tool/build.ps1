344134056ae019f4d663a5eb1873c14db398d458
C. Augusto Proiete <augusto@proiete.com>
2020-12-03 08:27:15 -0400

    Add initial build.ps1 bootstrapper for dotnet-tool

--- dotnet-tool/build.ps1
+++ dotnet-tool/build.ps1
@@ -1,1 +1,13 @@
-Write-Output "This is a work-in-progress bootstrapper for using the Cake Runner for .Net Core Tool"
+$ErrorActionPreference = 'Stop'
+
+Set-Location -LiteralPath $PSScriptRoot
+
+$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = '1'
+$env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
+$env:DOTNET_NOLOGO = '1'
+
+dotnet tool restore
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+dotnet cake @args
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
